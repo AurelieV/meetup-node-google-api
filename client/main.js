b6dbbff4c892c7a4f42986f5f6daca559e7f3341
Aurélie Violette <orwel.violette@gmail.com>
2015-12-20 20:13:16 +0100

    Use a var rather than bind in ctrl

--- client/main.js
+++ client/main.js
@@ -5,26 +5,28 @@ app.config(function($locationProvider){
 });
 
 app.controller('mainController', function(Event, GoogleAccount, $location, $scope) {
+  var ctrl = this;
+
   // -- Server side auth --
   this.events = Event.find();
   this.createEvent = function(event) {
     Event.create(event, function(eventCreated){
-      this.events.push(eventCreated);
-    }.bind(this))
+      ctrl.events.push(eventCreated);
+    })
   };
 
   // -- Client side auth --
   this.accounts = GoogleAccount.find();
 
   GoogleAccount.getUrl(function(data){
-    this.authUrl = data.url;
-  }.bind(this));
+    ctrl.authUrl = data.url;
+  });
 
   // Handle return from Google
   var code = $location.search().code;
   if (code) {
     GoogleAccount.loadCalendars({code: code}, function(data) {
-      this.calendars = data.calendars
-    }.bind(this))
+      ctrl.calendars = data.calendars
+    })
   }
 });
