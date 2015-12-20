68fb7e33015f72d523c15ce089f2328f6cc7c3f1
Aurélie Violette <orwel.violette@gmail.com>
2015-12-20 20:24:01 +0100

    Clarify a bit the code

--- common/models/event.js
+++ common/models/event.js
@@ -5,20 +5,17 @@ module.exports = function(Event) {
   Event.beforeRemote('create', function(ctx, event, next) {
     var body = ctx.req.body;
     var date = moment(body.date).format('YYYY-MM-DD');
-    calendar.events.insert({
-        calendarId: 'p5cg5s4qnfelvk9r0hkil8q7p0@group.calendar.google.com',
-        resource: {
-          summary: body.title,
-          end: {
-            date: date
-          },
-          start: {
-            date: date
-          }
-        }
-      }, function (err, googleEvent) {
-        body.googleId = googleEvent.id;
-        next();
+    var options = {
+      calendarId: 'p5cg5s4qnfelvk9r0hkil8q7p0@group.calendar.google.com',
+      resource: {
+        summary: body.title,
+        end: {date: date},
+        start: {date: date}
+      }
+    };
+    calendar.events.insert(options, function (err, googleEvent) {
+      body.googleId = googleEvent.id;
+      next();
     });
   });
 };
